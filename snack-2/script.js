ce9962841e9a0e097f7e958233780f45b320d0db
EneaSema <sema.enea1993@gmail.com>
2025-04-22 15:42:30 +0200

    problem-exercise-two

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,24 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+console.log(people);
+
+for (let i = 0; i < people.length; i++) {
+  let currentElementName = "";
+  currentElementName = `${currentElementName} people[i].name`;
+  console.log(currentElement);
+}
+console.log(currentElement);
+
+// const namePeople = people.filter((person) => {
+//   currentName = person.name;
+//   return currentName;
+// });
+
+// console.log(namePeople);
