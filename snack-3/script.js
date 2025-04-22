2cb02b41b88c3eb63b3159477b2990801bd97f73
EneaSema <sema.enea1993@gmail.com>
2025-04-22 16:48:16 +0200

    resolve-exercise-third

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const newNumbers = numbers.map(function (current) {
+  current = 5;
+  console.log(current);
+  return current;
+});
+
+console.log(numbers);
+console.log(newNumbers);
